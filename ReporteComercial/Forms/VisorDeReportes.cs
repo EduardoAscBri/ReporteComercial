0d9e56dea6e3b5fcec58b2d900eb7ff89d13c23e
Eduardo Ascencio Bribiesca <eduardoascbri@outlook.com>
2019-12-09 11:25:20 -0600

    Commit Inicial

---
+++ ReporteComercial/Forms/VisorDeReportes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReporteComercial.Forms
+{
+    public partial class VisorDeReportes : Form
+    {
+        public VisorDeReportes()
+        {
+            InitializeComponent();
+        }
+
+        private void VisorDeReportes_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer2.RefreshReport();
+        }
+    }
+}
