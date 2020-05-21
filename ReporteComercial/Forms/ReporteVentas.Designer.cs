56ba6d7d4b476c068ed080f3359755ee3b873ea2
Eduardo Ascencio Bribiesca <eduardoascbri@outlook.com>
2020-05-20 20:04:07 -0500

    Version actual 20 - 05 - 2020
    Estable

--- ReporteComercial/Forms/ReporteVentas.Designer.cs
+++ ReporteComercial/Forms/ReporteVentas.Designer.cs
@@ -28,12 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReporteVentas));
             this.dtpFechaInicial = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.dtpFechaFinal = new System.Windows.Forms.DateTimePicker();
             this.bttAcept = new System.Windows.Forms.Button();
             this.bttSalir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbDoctoCargo = new System.Windows.Forms.ComboBox();
+            this.cmbDoctoAbono = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dtpFechaInicial
@@ -41,7 +49,7 @@
             this.dtpFechaInicial.CalendarFont = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpFechaInicial.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpFechaInicial.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpFechaInicial.Location = new System.Drawing.Point(65, 62);
+            this.dtpFechaInicial.Location = new System.Drawing.Point(326, 163);
             this.dtpFechaInicial.Name = "dtpFechaInicial";
             this.dtpFechaInicial.Size = new System.Drawing.Size(264, 25);
             this.dtpFechaInicial.TabIndex = 0;
@@ -50,7 +58,8 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(132, 24);
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(393, 125);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(118, 25);
             this.label1.TabIndex = 1;
@@ -60,7 +69,8 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(134, 112);
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(395, 213);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 25);
             this.label2.TabIndex = 3;
@@ -71,7 +81,7 @@
             this.dtpFechaFinal.CalendarFont = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpFechaFinal.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtpFechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpFechaFinal.Location = new System.Drawing.Point(65, 153);
+            this.dtpFechaFinal.Location = new System.Drawing.Point(326, 254);
             this.dtpFechaFinal.Name = "dtpFechaFinal";
             this.dtpFechaFinal.Size = new System.Drawing.Size(264, 25);
             this.dtpFechaFinal.TabIndex = 2;
@@ -81,7 +91,7 @@
             this.bttAcept.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.bttAcept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bttAcept.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bttAcept.Location = new System.Drawing.Point(130, 219);
+            this.bttAcept.Location = new System.Drawing.Point(352, 300);
             this.bttAcept.Name = "bttAcept";
             this.bttAcept.Size = new System.Drawing.Size(120, 32);
             this.bttAcept.TabIndex = 5;
@@ -94,7 +104,7 @@
             this.bttSalir.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.bttSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bttSalir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bttSalir.Location = new System.Drawing.Point(263, 219);
+            this.bttSalir.Location = new System.Drawing.Point(485, 300);
             this.bttSalir.Name = "bttSalir";
             this.bttSalir.Size = new System.Drawing.Size(120, 32);
             this.bttSalir.TabIndex = 6;
@@ -102,21 +112,89 @@
             this.bttSalir.UseVisualStyleBackColor = false;
             this.bttSalir.Click += new System.EventHandler(this.bttSalir_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(50, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Documento de cargo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.LightGray;
+            this.label4.Location = new System.Drawing.Point(50, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(207, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Documento de abono";
+            // 
+            // cmbDoctoCargo
+            // 
+            this.cmbDoctoCargo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDoctoCargo.FormattingEnabled = true;
+            this.cmbDoctoCargo.Location = new System.Drawing.Point(31, 163);
+            this.cmbDoctoCargo.Name = "cmbDoctoCargo";
+            this.cmbDoctoCargo.Size = new System.Drawing.Size(241, 25);
+            this.cmbDoctoCargo.TabIndex = 9;
+            // 
+            // cmbDoctoAbono
+            // 
+            this.cmbDoctoAbono.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDoctoAbono.FormattingEnabled = true;
+            this.cmbDoctoAbono.Location = new System.Drawing.Point(31, 254);
+            this.cmbDoctoAbono.Name = "cmbDoctoAbono";
+            this.cmbDoctoAbono.Size = new System.Drawing.Size(241, 25);
+            this.cmbDoctoAbono.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ReporteComercial.Properties.Resources.Matriz_Logo_Color;
+            this.pictureBox1.Location = new System.Drawing.Point(81, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.LightGray;
+            this.label5.Location = new System.Drawing.Point(319, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 37);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Reporte comercial";
+            // 
             // ReporteVentas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(399, 263);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.ClientSize = new System.Drawing.Size(617, 346);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmbDoctoAbono);
+            this.Controls.Add(this.cmbDoctoCargo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.bttSalir);
             this.Controls.Add(this.bttAcept);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dtpFechaFinal);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtpFechaInicial);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ReporteVentas";
-            this.Text = "Reporte de ventas";
-            this.Load += new System.EventHandler(this.ReporteVentas_Load);
+            this.Text = "Reporte comercial";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,5 +208,11 @@
         private System.Windows.Forms.DateTimePicker dtpFechaFinal;
         private System.Windows.Forms.Button bttAcept;
         private System.Windows.Forms.Button bttSalir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbDoctoCargo;
+        private System.Windows.Forms.ComboBox cmbDoctoAbono;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
--- ReporteComercial/Forms/ReporteVentas.cs
+++ ReporteComercial/Forms/ReporteVentas.cs
@@ -21,14 +21,37 @@ namespace ReporteComercial.Forms
         public ReporteVentas()
         {
             InitializeComponent();
+            ConectaBaseDatos();
+            CargarDocumentosModelo();
         }
 
-        private void ReporteVentas_Load(object sender, EventArgs e)
+        public void ConectaBaseDatos()
         {
             this.rutaLocal = Application.StartupPath;
             InnerDatabaseManager databaseManager = new InnerDatabaseManager();
             this.conexion = databaseManager.createConnectionFromIniFile(this.rutaLocal);
+            //this.conexion.Open();
         }
+
+        public void CargarDocumentosModelo()
+        {
+            SqlCommand comandoModelo = new SqlCommand("SELECT admDocumentosModelo.CIDDOCUMENTODE, admDocumentosModelo.CDESCRIPCION FROM admDocumentosModelo", this.conexion);
+            comandoModelo.CommandType = CommandType.Text;
+            SqlDataAdapter dataAdapterModelo = new SqlDataAdapter(comandoModelo);
+            DataTable tablaCargo = new DataTable();
+            DataTable tablaAbono = new DataTable();
+            dataAdapterModelo.Fill(tablaCargo);
+            dataAdapterModelo.Fill(tablaAbono);
+
+            this.cmbDoctoCargo.DataSource = tablaCargo;
+            this.cmbDoctoCargo.DisplayMember = "CDESCRIPCION";
+            this.cmbDoctoCargo.ValueMember = "CIDDOCUMENTODE";
+
+            this.cmbDoctoAbono.DataSource = tablaAbono;
+            this.cmbDoctoAbono.DisplayMember = "CDESCRIPCION";
+            this.cmbDoctoAbono.ValueMember = "CIDDOCUMENTODE";
+        }
+
 
         private void bttSalir_Click(object sender, EventArgs e)
         {
@@ -41,13 +64,19 @@ namespace ReporteComercial.Forms
             string lFechaInicial = "";
             string lFechaFinal = "";
 
+            int lDoctoCargo = 0;
+            int lDoctoAbono = 0;
+
             lFechaInicial = this.dtpFechaInicial.Text;
             lFechaFinal = this.dtpFechaFinal.Text;
 
-            reporte(lFechaInicial, lFechaFinal);
+            lDoctoCargo = Convert.ToInt32(this.cmbDoctoCargo.SelectedValue);
+            lDoctoAbono = Convert.ToInt32(this.cmbDoctoAbono.SelectedValue);
+
+            reporte(lDoctoCargo, lDoctoAbono, lFechaInicial, lFechaFinal);
         }
 
-        public void reporte(string fechaInicial, string fechaFinal)
+        public void reporte(int doctoCargo, int doctoAbono, string fechaInicial, string fechaFinal)
         {
             DataTable reporte = new DataTable();
             reporte.Columns.Add("CSERIEDOCUMENTO");
@@ -86,6 +115,7 @@ namespace ReporteComercial.Forms
             reporte.Columns.Add("CDESCUENTO1");
             reporte.Columns.Add("CDESCUENTO2");
             reporte.Columns.Add("CTOTAL");
+            reporte.Columns.Add("CCOSTOESPECIFICO");
             reporte.Columns.Add("CREFERENCIA");
             reporte.Columns.Add("COBSERVAMOV");
             reporte.Columns.Add("SERIEPAGO");
@@ -93,10 +123,12 @@ namespace ReporteComercial.Forms
             reporte.Columns.Add("FECHAPAGO");
             reporte.Columns.Add("IMPORTEPAGADO");
 
+            //Se modifica el CIDDOCUMENTODE, de (4 facturas) a (19 compras)
             string cmdFacturas = "SELECT CIDDOCUMENTO, CSERIEDOCUMENTO, CFOLIO, CFECHA " +
                 "FROM admDocumentos " +
-                "WHERE(admDocumentos.CIDDOCUMENTODE = 4) " +
+                "WHERE(admDocumentos.CIDDOCUMENTODE = @doctoCargo) " +
                 "AND (admDocumentos.CFECHA BETWEEN @fechaInicial AND @fechaFinal)";
+
 
             string cmdFacturaDetalle = "SELECT        admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, " +
                 "admClientes.CCODIGOCLIENTE, admClientes.CRAZONSOCIAL, admClientes.CRFC, " +
@@ -111,7 +143,7 @@ namespace ReporteComercial.Forms
                 "admClasificacionesValores_5.CVALORCLASIFICACION AS TIPO, " +
                 "admMovimientos.CNUMEROMOVIMIENTO, admMovimientos.CUNIDADES, admMovimientos.CPRECIO, admMovimientos.CNETO, " +
                 "admMovimientos.CDESCUENTO1, admMovimientos.CDESCUENTO2, " +
-                "admMovimientos.CTOTAL, " +
+                "admMovimientos.CTOTAL, admMovimientos.CCOSTOESPECIFICO, " +
                 "admMovimientos.CREFERENCIA, admMovimientos.COBSERVAMOV " +
                 "FROM admDocumentos " +
                 "LEFT JOIN admClientes " +
@@ -134,22 +166,24 @@ namespace ReporteComercial.Forms
                 "ON admProductos.CIDVALORCLASIFICACION4 = admClasificacionesValores_4.CIDVALORCLASIFICACION " +
                 "LEFT JOIN admClasificacionesValores AS admClasificacionesValores_5 " +
                 "ON admProductos.CIDVALORCLASIFICACION5 = admClasificacionesValores_5.CIDVALORCLASIFICACION " +
-                "WHERE(admDocumentos.CIDDOCUMENTODE = 4 AND admDocumentos.CCANCELADO = 0) AND (admDocumentos.CIDDOCUMENTO = @cidfactura) AND (admDomicilios.CTIPOCATALOGO = 1) AND(admDomicilios.CTIPODIRECCION = 0) " +
+                "WHERE(admDocumentos.CIDDOCUMENTODE = @doctoCargo AND admDocumentos.CCANCELADO = 0) AND (admDocumentos.CIDDOCUMENTO = @cidfactura) AND (admDomicilios.CTIPOCATALOGO = 1) AND(admDomicilios.CTIPODIRECCION = 0) " +
                 "ORDER BY admDocumentos.CFOLIO ASC";
 
+            //Se cambio el CIDDOCUMENTODE de (9 pago del ciente) a (26 abono a proveedor)
             string cmdCargosAbonos = "SELECT        admAsocCargosAbonos.CIDDOCUMENTOABONO, admAsocCargosAbonos.CIDDOCUMENTOCARGO, " +
                 "admAsocCargosAbonos.CIMPORTEABONO, admAsocCargosAbonos.CIMPORTECARGO, " +
                 "admDocumentos.CSERIEDOCUMENTO, admDocumentos.CFOLIO, admDocumentos.CFECHA, admDocumentos.CTOTAL " +
                 "FROM admAsocCargosAbonos " +
                 "LEFT JOIN admDocumentos " +
                 "ON admAsocCargosAbonos.CIDDOCUMENTOABONO = admDocumentos.CIDDOCUMENTO " +
-                "WHERE(admDocumentos.CIDDOCUMENTODE = 9) AND (admAsocCargosAbonos.CIDDOCUMENTOCARGO = @cidfactura) AND (admDocumentos.CFECHA BETWEEN @fechaInicial AND @fechaFinal) AND (admDocumentos.CCANCELADO = 0)";
+                "WHERE(admDocumentos.CIDDOCUMENTODE = @doctoAbono) AND (admAsocCargosAbonos.CIDDOCUMENTOCARGO = @cidfactura) AND (admDocumentos.CFECHA BETWEEN @fechaInicial AND @fechaFinal) AND (admDocumentos.CCANCELADO = 0)";
 
             SqlCommand command = new SqlCommand();
             command = new SqlCommand(cmdFacturas, this.conexion);
-            
+
             command.Parameters.AddWithValue("@fechaInicial", fechaInicial);
             command.Parameters.AddWithValue("@fechaFinal", fechaFinal);
+            command.Parameters.AddWithValue("@doctoCargo", doctoCargo);
             
             SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
             DataTable listaFacturas = new DataTable();
@@ -158,7 +192,6 @@ namespace ReporteComercial.Forms
             DataTable detalleFactura = new DataTable();
             DataTable detallePago = new DataTable();
 
-            
 
             foreach(DataRow row in listaFacturas.Rows)
             {
@@ -168,6 +201,7 @@ namespace ReporteComercial.Forms
                 detalleFactura.Clear();
                 command = new SqlCommand(cmdFacturaDetalle, this.conexion);
                 command.Parameters.AddWithValue("@cidfactura", cidfactura);
+                command.Parameters.AddWithValue("@doctoCargo", doctoCargo);
                 dataAdapter = new SqlDataAdapter(command);
                 dataAdapter.Fill(detalleFactura);
 
@@ -177,6 +211,7 @@ namespace ReporteComercial.Forms
                 command.Parameters.AddWithValue("@cidfactura", cidfactura);
                 command.Parameters.AddWithValue("@fechaInicial", fechaInicial);
                 command.Parameters.AddWithValue("@fechaFinal", fechaFinal);
+                command.Parameters.AddWithValue("@doctoAbono", doctoAbono);
                 dataAdapter = new SqlDataAdapter(command);
                 dataAdapter.Fill(detallePago);
 
@@ -197,9 +232,12 @@ namespace ReporteComercial.Forms
                 }
                 else if (pagos > 1)
                 {
+                    DataRow rowPago = detallePago.Rows[(pagos-1)];
                     seriePago = "VARIOS";
-                    folioPago = 0;
-                    fechaPago = DateTime.Now;
+                    //folioPago = 0;
+                    folioPago = Convert.ToInt32(rowPago["CFOLIO"]);
+                    //fechaPago = DateTime.Now;
+                    fechaPago = Convert.ToDateTime(rowPago["CFECHA"]);
                 }
 
 
@@ -210,7 +248,7 @@ namespace ReporteComercial.Forms
                 //Resume el total de la factura segun los movimientos
                 foreach (DataRow rowDetalle in detalleFactura.Rows)
                 {
-                    totalFactura = totalFactura + Convert.ToDouble(rowDetalle["CTOTAL"]);
+                    totalFactura = totalFactura + Convert.ToDouble(rowDetalle["CTOTAL"] is DBNull ? 0 : rowDetalle["CTOTAL"]);
                 }
                 //Resume el total pagado segun los abonos aplicados
                 foreach(DataRow rowDetalle in detallePago.Rows)
@@ -238,7 +276,7 @@ namespace ReporteComercial.Forms
                     }
                     */
 
-                    double totalMovimiento = Convert.ToDouble(rowDetalle["CTOTAL"]);
+                    double totalMovimiento = Convert.ToDouble(rowDetalle["CTOTAL"] is DBNull ? 0 : rowDetalle["CTOTAL"]);
                     if (totalPago >= totalMovimiento)
                     {
                         importePagado = totalMovimiento;
@@ -289,6 +327,7 @@ namespace ReporteComercial.Forms
                     dataRow["CDESCUENTO1"] = rowDetalle["CDESCUENTO1"];
                     dataRow["CDESCUENTO2"] = rowDetalle["CDESCUENTO2"];
                     dataRow["CTOTAL"] = rowDetalle["CTOTAL"];
+                    dataRow["CCOSTOESPECIFICO"] = rowDetalle["CCOSTOESPECIFICO"];
                     dataRow["CREFERENCIA"] = rowDetalle["CREFERENCIA"];
                     dataRow["COBSERVAMOV"] = rowDetalle["COBSERVAMOV"];
                     dataRow["SERIEPAGO"] = seriePago;
